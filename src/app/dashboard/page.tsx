6a167eb6f4772a0aabc64b17a8c88eda05de65af
ourstudyai <ourstudyai@gmail.com>
2026-05-25 10:33:19 +0100

    Fix JSX structure by closing fragments correctly

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -364,8 +364,8 @@ export default function DashboardPage() {
               </div>
             </>
           )}
-          </div>
+          </>
+      )}
+    </div>
   );
 }
-
-                                            
